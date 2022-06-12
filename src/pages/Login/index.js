019ef0b2335232f4ac4e4f6fe6121be46be57fb8
Pablo Salas <pablosalas@outlook.com.ar>
2022-06-12 16:47:00 +0200

    add login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,46 @@
+import { useContext, useState } from "react";
+
+import { login } from "../../services/auth";
+import Context from "../../context/UserContext";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { token, setToken } = useContext(Context);
+
+    const handleEmailChange = ({ target }) => setEmail(target.value);
+    const handlePasswordChange = ({ target }) => setPassword(target.value);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        login(email, password)
+            .then(res => setToken(res))
+            .catch(err => console.log(err));
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="email">email:</label>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="ejemplo@correo.com"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">contraseña:</label>
+                <input
+                    id="password"
+                    type="password"
+                    placeholder="Ingrese su contraseña..."
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </div>
+            <button>Iniciar sesión</button>
+        </form>
+    );
+}
