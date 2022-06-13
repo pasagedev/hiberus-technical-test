e5ebb7411601e92abb79322a173d7afdb75d57a8
Pablo Salas <pablosalas@outlook.com.ar>
2022-06-13 22:29:37 +0100

    add Header component to App

--- src/App.js
+++ src/App.js
@@ -4,28 +4,42 @@ import {
     Routes,
     Navigate,
 } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Container, Row, Col } from "react-bootstrap";
 
 import Login from "./pages/Login";
 import { UserContextProvider } from "./context/UserContext";
 import Users from "./pages/Users";
 import Logout from "./pages/Logout";
 import SingUp from "./pages/SingUp";
+import Header from "./components/Header";
 
 function App() {
     return (
-        <div className="App">
-            <UserContextProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/register" element={<SingUp />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/logout" element={<Logout />} />
-                        <Route path="/users" element={<Users />} />
-                        <Route path="/" element={<Navigate to="/users" />} />
-                    </Routes>
-                </Router>
-            </UserContextProvider>
-        </div>
+        <Container fluid>
+            <Col>
+                <UserContextProvider>
+                    <Router>
+                        <Row className="mt-2">
+                            <Header />
+                        </Row>
+                        <Row className="mt-3">
+                            <Routes>
+                                <Route path="/register" element={<SingUp />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/logout" element={<Logout />} />
+                                <Route path="/users" element={<Users />} />
+                                <Route
+                                    path="/"
+                                    element={<Navigate to="/users" />}
+                                />
+                            </Routes>
+                        </Row>
+                    </Router>
+                </UserContextProvider>
+            </Col>
+        </Container>
     );
 }
 
