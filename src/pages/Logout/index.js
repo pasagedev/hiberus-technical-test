11615aeeaaaae21aa3e9fec39f3b5dbef75652d2
Pablo Salas <pablosalas@outlook.com.ar>
2022-06-14 06:29:50 +0100

    fix styles on Logout page

--- src/pages/Logout/index.js
+++ src/pages/Logout/index.js
@@ -1,12 +1,38 @@
-import { useContext } from "react";
+import { Button, Row, Col } from "react-bootstrap";
+import { useContext, useState } from "react";
 import Context from "../../context/UserContext";
 
+import Notification from "../../components/Notification";
+
 export default function Logout() {
-    const { setToken } = useContext(Context);
+    const { token, setToken } = useContext(Context);
+    const [message, setMessage] = useState(false);
 
     const handleClick = () => {
+        setMessage({ text: "Sesión terminada", type: "success" });
         setToken(null);
         localStorage.removeItem("token");
     };
-    return <button onClick={handleClick}>Cerrar sesión</button>;
+
+    return (
+        <Col className="mx-auto">
+            <Row>
+                {message && (
+                    <Notification text={message.text} type={message.type} />
+                )}
+            </Row>
+            <Row className="mb-3">
+                <h2 className="text-center">Logout</h2>
+            </Row>
+            <Row sm={2} className="justify-content-center">
+                <Button
+                    variant="danger"
+                    onClick={handleClick}
+                    disabled={token ? false : true}
+                >
+                    Cerrar sesión
+                </Button>
+            </Row>
+        </Col>
+    );
 }
